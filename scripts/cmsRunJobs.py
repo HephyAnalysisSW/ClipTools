afa70054c73c0741777feb27710ac33925d34c12
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2021-01-24 00:12:10 +0100

    cmsRun jobs over directory

--- scripts/cmsRunJobs.py
+++ scripts/cmsRunJobs.py
@@ -5,14 +5,13 @@ def get_parser():
     argParser = argparse.ArgumentParser(description = "Argument parser for SLURM cmsRun submission")
 
     argParser.add_argument('--logLevel',    action='store',         nargs='?',  choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'],   default='INFO', help="Log level for logging" )
-    argParser.add_argument('--DASName',     action='store',         nargs='?',  type=str, required=True, help="DAS Name of the sample." )
+    argParser.add_argument('--input',       action='store',         nargs='?',  type=str, required=True, help="dbs:<DAS name> or directory" )
     argParser.add_argument('--instance',    action='store',         nargs='?',  type=str, default='global', help="DAS instance." )
     argParser.add_argument('--redirector',  action='store',         nargs='?',  type=str, default='root://cms-xrd-global.cern.ch/', help="redirector for xrootd" )
     argParser.add_argument('--targetDir',   action='store',         nargs='?',  type=str, required=True, help="output director" )
     argParser.add_argument('--cfg',         action='store',         nargs='?',  type=str, help="Which config." )
     argParser.add_argument('--limit',       action='store',         nargs='?',  type=int, default=0, help="Limit DAS query?" )
     argParser.add_argument('--split',       action='store',         nargs='?',  type=int, help="Number of jobs." )
-    #argParser.add_argument('--targetDir',   action='store',         nargs='?',  type=str, default=user.postprocessing_output_directory, help="Name of the directory the post-processed files will be saved" )
 
     return argParser
 
@@ -25,24 +24,38 @@ import os
 import logger
 logger = logger.get_logger( args.logLevel, logFile=None )
 
-def _dasPopen(dbs):
-    logger.info('DAS query\t: %s',  dbs)
-    return os.popen(dbs)
-
-query, qwhat = args.DASName, "dataset"
-if "#" in args.DASName: qwhat = "block"
-
 # Deal with the sample
-logger.info("Sample: %s", args.DASName)
-
-dbs='dasgoclient -query="file %s=%s instance=prod/%s" --limit %i'%(qwhat,query, args.instance, args.limit)
-dbsOut = _dasPopen(dbs).readlines()
-
 files = []
-for line in dbsOut:
-    if line.startswith('/store/'):
-        files.append(line.rstrip())
+# get from dbs
+if args.input.startswith('dbs:'):
+    DASName = args.input[4:] 
+    # name for the subdirectory
+    subDirName = DASName.lstrip('/').replace('/','_')
+    def _dasPopen(dbs):
+        logger.info('DAS query\t: %s',  dbs)
+        return os.popen(dbs)
 
+    query, qwhat = DASName, "dataset"
+    if "#" in DASName: qwhat = "block"
+
+    logger.info("Sample: %s", DASName)
+
+    dbs='dasgoclient -query="file %s=%s instance=prod/%s" --limit %i'%(qwhat,query, args.instance, args.limit)
+    dbsOut = _dasPopen(dbs).readlines()
+
+    for line in dbsOut:
+        if line.startswith('/store/'):
+            files.append(line.rstrip())
+#get from directory
+else:
+    subDirName = ''
+    for filename in os.listdir( args.input ):
+        if filename.endswith('.root'):
+            files.append(os.path.join( args.input, filename ))
+
+if len(files)==0:
+    raise RuntimeError('Found zero files for input %s'%args.input)
+    
 def partition(lst, n):
     ''' Partition list into chunks of approximately equal size'''
     # http://stackoverflow.com/questions/2659900/python-slicing-a-list-into-n-nearly-equal-length-partitions
@@ -57,18 +70,17 @@ logger.info( "Got %i files and split into %i jobs of %3.2f files/job on average.
 for chunk in chunks:
     pass
 
-
 # Deal with the config
 logger.info("Config: %s", args.cfg)
-import importlib
+import imp
 if os.path.exists( args.cfg ):
-    module = importlib.import_module(os.path.expandvars(args.cfg.rstrip('.py')))
+    module = imp.load_source('process_tmp', os.path.expandvars(args.cfg))
     logger.info( "Loaded config" )
 else:
     logger.error( "Did not find cfg %s", args.cfg )
     sys.exit(-1)
 
-targetDir = os.path.join( args.targetDir, args.DASName.lstrip('/').replace('/','_') )
+targetDir = os.path.join( args.targetDir, subDirName )
 if not os.path.exists( targetDir ):
     os.makedirs( targetDir )
     logger.info( 'Created output directory %s', targetDir )
@@ -90,12 +102,13 @@ with file('jobs.sh', 'a+') as job_file:
         move_cmds = []
         # set output
         for out_name, output_module in module.process.outputModules.iteritems():
-            output_filename = '%s_%i.root'%(out_name, i_chunk)
-            output_module.fileName  = cms.untracked.string(os.path.join('/tmp/', output_filename))
-            move_cmds.append( (os.path.join('/tmp/', output_filename), os.path.join(targetDir, output_filename)) )
+            output_filename     = '%s_%i.root'%(out_name, i_chunk)
+            output_tmp_filename = '%s_%i_%s.root'%(out_name, i_chunk, str(uuid.uuid4()))
+            output_module.fileName  = cms.untracked.string(os.path.join('/tmp/', output_tmp_filename))
+            move_cmds.append( (os.path.join('/tmp/', output_tmp_filename), os.path.join(targetDir, output_filename)) )
         # set maxEvents to -1
         if hasattr( module.process, "maxEvents" ):
-            module.process.maxEvents.input = cms.untracked.int32(1)
+            module.process.maxEvents.input = cms.untracked.int32(-1)
         # dump cfg
         out_cfg_name = os.path.join( batch_tmp, str(uuid.uuid4()).replace('-','_')+'.py' )
         with file(out_cfg_name, 'w') as out_cfg:
